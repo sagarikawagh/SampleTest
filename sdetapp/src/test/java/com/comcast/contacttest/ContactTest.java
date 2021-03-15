0260ddd73c19c8364372d02f5e7b538f4cbfd4ba
Admin <Admin@DESKTOP-CPLEQ0J>
2021-03-15 15:44:11 -0700

    First Committe

---
+++ sdetapp/src/test/java/com/comcast/contacttest/ContactTest.java
@@ -0,0 +1,24 @@
+package com.comcast.contacttest;
+
+import org.testng.annotations.Test;
+
+public class ContactTest
+{
+	@Test
+	public void createcontacttest()
+	{
+		System.out.println("execute createcontacttest");
+	}
+	
+	@Test
+	public void modifycontacttest()
+	{
+		System.out.println("execute modifycontacttest");
+	}
+	
+	public void deletecontacttest()
+	{
+		System.out.println("execute deletecontacttest");
+	}
+
+}
