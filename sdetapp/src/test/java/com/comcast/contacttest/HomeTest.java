2b72899f0d1345947fd59f1ebb0dc1b13c3b725f
Admin <Admin@DESKTOP-CPLEQ0J>
2021-03-15 15:53:02 -0700

    add home page

---
+++ sdetapp/src/test/java/com/comcast/contacttest/HomeTest.java
@@ -0,0 +1,12 @@
+package com.comcast.contacttest;
+
+import org.testng.annotations.Test;
+
+public class HomeTest 
+{
+  @Test
+  public void HomeTest()
+	{
+		System.out.println("execut HomeTest");
+	}
+}
